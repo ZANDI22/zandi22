0739ee1585a1bc946ed7fa5c84841335b4b78674
ddmkim94 <ddmkim94@gmail.com>
2022-11-22 13:23:36 +0900

    :sparkles: feat: 리액트, 스프링부트 연결

---
+++ src/main/java/com/yemin/zandi22/home/HomeController.java
@@ -0,0 +1,15 @@
+package com.yemin.zandi22.home;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "동민+예현";
+    }
+}
