cce9c5261eebf662c6e26d9f27d51649a34a5fab
ddmkim94 <ddmkim94@gmail.com>
2022-11-22 12:39:08 +0900

    create project

---
+++ src/main/java/com/yemin/zandi22/Zandi22Application.java
@@ -0,0 +1,13 @@
+package com.yemin.zandi22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zandi22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zandi22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yemin/zandi22/Zandi22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yemin.zandi22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zandi22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
